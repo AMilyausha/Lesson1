84084b2e897896f750e3b66fc6eb7cb9e0ac051c
Amirova Milyausha <mila17.86@mail.ru>
2022-08-24 21:38:50 +0300

    Первое задание

---
+++ Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿int a,b;
+Console.Write("Введите первое число ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+b= int.Parse(Console.ReadLine());
+if (a > b){
+    Console.WriteLine($"Max {a}");
+    Console.WriteLine($"Min {b}");
+    }
+else{
+    Console.WriteLine($"Max {b}");
+    Console.WriteLine($"Min {a}");
+    }
