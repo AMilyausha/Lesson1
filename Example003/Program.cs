332444968bc23e551b2d229a99a42a78d3ab3d81
Amirova Milyausha <mila17.86@mail.ru>
2022-08-24 22:20:06 +0300

    Третье задание

---
+++ Example003/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+Console.Write("Введите число: ");
+a=int.Parse(Console.ReadLine());
+if (a % 2 ==0){
+    Console.Write ("Yes");
+    }
+    else
+    {
+       Console.Write ("No"); 
+    }
+
+
+
